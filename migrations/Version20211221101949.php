ca2abfeee6e8152a27aa1eda354cd283a8924fe5
dgos <dawid.gos@coig.pl>
2021-12-21 11:54:29 +0100

    Added ORM OneToMany user->posts $user column.

--- migrations/Version20211221101949.php
+++ migrations/Version20211221101949.php
@@ -19,7 +19,6 @@ final class Version20211221101949 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE post ADD usr_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8DC69D3FB FOREIGN KEY (usr_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('CREATE INDEX IDX_5A8A6C8DC69D3FB ON post (usr_id)');
@@ -27,7 +26,6 @@ final class Version20211221101949 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-
         $this->addSql('ALTER TABLE post DROP CONSTRAINT FK_5A8A6C8DC69D3FB');
         $this->addSql('DROP INDEX IDX_5A8A6C8DC69D3FB');
         $this->addSql('ALTER TABLE post DROP usr_id');
